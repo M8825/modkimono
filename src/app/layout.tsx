78cf05fe3e0d84441af39983b9499238f281f051
Mlkz <maho.mamulashvili@gmail.com>
2024-03-02 12:23:45 -0500

    Update waitlist component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,24 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import React from 'react';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Mod Kimono',
+};
+
+interface RootLayoutProps {
+  children: React.ReactNode;
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
-}
+  );
+};
+
+export default RootLayout;
